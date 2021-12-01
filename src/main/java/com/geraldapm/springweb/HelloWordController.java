0c980d737b0b6224d6bc4f91a0a5198b0cb3d4e8
Geraldhi Mahayadnya <geraldiaditya1111@gmail.com>
2021-12-01 21:05:45 +0700

    init project

---
+++ src/main/java/com/geraldapm/springweb/HelloWordController.java
@@ -0,0 +1,11 @@
+package com.geraldapm.springweb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/helloWorld")
+public class HelloWordController {
+
+}
