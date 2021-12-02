9a050592200df0433db7d1fbeebd9cd8c9c6320e
Geraldhi Mahayadnya <geraldiaditya1111@gmail.com>
2021-12-02 19:03:58 +0700

    init project #3

---
+++ demo-app/src/main/java/com/geraldapm/springweb/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.geraldapm.springweb;
+
+public class HelloWorld {
+    private String hello = "world";
+
+    public HelloWorld() {
+
+    }
+
+    public String getHello() {
+        return hello;
+    }
+
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+}
